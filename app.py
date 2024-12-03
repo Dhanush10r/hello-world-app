37b241c184e409c9611ebd5467f76990326e4952
Dhanush10r <dhanushdhanu10r@gmail.com>
2024-12-03 15:23:39 +0530

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "Hello, World!"
+def home():
+    return render_template('home.html')
+
+@app.route('/greet', methods=['POST'])
+def greet():
+    name = request.form['name']
+    return render_template('greet.html', name=name)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
